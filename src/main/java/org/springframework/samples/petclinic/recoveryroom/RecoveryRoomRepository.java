3de92e7721e1d9811a5aae9988fb21b634343767
Carlos <carlosvsoult@gmail.com>
2022-01-14 16:45:13 +0100

    Git push examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,11 +3,28 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer> {
+
     List<RecoveryRoom> findAll();
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    
+    @Query("SELECT r FROM RecoveryRoomType r")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
+
+    @Query("SELECT r FROM RecoveryRoomType r WHERE r.name=:roomType")
+    RecoveryRoomType findRecoveryRoomTypeByName(@Param("roomType") String roomType);
+
+    @Query("SELECT r FROM RecoveryRoom r WHERE r.size>:tam")
+    List<RecoveryRoom> findBySizeMoreThan(@Param("tam") double tam);
+    
     Optional<RecoveryRoom> findById(int id);
+
+    @SuppressWarnings("unchecked")
     RecoveryRoom save(RecoveryRoom p);
+    
     //RecoveryRoomType getRecoveryRoomType(String name);
     //List<RecoveryRoom> findBySizeMoreThan(double size);
 }
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -62,3 +62,10 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02',
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
 
+INSERT INTO recovery_room_type(id,name) VALUES (1,'room');
+INSERT INTO recovery_room_type(id,name) VALUES (2,'box');
+
+INSERT INTO recovery_room(id,name,size,secure,room_type) VALUES (1,'Big room for dangerous animals',6.50,true,1);
+INSERT INTO recovery_room(id,name,size,secure,room_type) VALUES (2,'Medium box',1.50,false,2);
+
+INSERT INTO visits(id,pet_id,visit_date,description,recovery_room) VALUES (5,2,'2021-01-04','spayed',1);
